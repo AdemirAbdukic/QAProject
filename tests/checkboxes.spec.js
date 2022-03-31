cee8559f705aaa3aaed0b60df0ed3cd4faef5b41
AdemirAbdukic <ademirabdukic@outlook.com>
2022-04-01 00:22:22 +0200

    Checkboxes

--- tests/checkboxes.spec.js
+++ tests/checkboxes.spec.js
@@ -2,6 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Checkboxes', async ({ page }) => {
   await page.goto('');
+  await expect(page).toHaveURL('https://the-internet.herokuapp.com');
   await page.pause();
   await page.locator('text=Checkboxes').click();
   await expect(page).toHaveURL('https://the-internet.herokuapp.com/checkboxes');
